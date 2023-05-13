fa39550e0a2bbc87a4a44c03d7953bc9b39d5f62
Maxim Mineev <mms1401@ya.ru>
2023-05-13 20:22:46 +0500

    add FindConnectedComponent

---
+++ GraphExtensions.cs
@@ -0,0 +1,16 @@
+﻿public static class GraphExtensions
+{
+    public static IEnumerable<IEnumerable<Node>> FindConnectedComponents(this Graph graph)
+    {
+        var marked = new HashSet<Node>();
+        while (true)
+        {
+            var node = graph.Nodes.Where(z => !marked.Contains(z)).FirstOrDefault();
+            if (node == null) break;
+            var breadthSearch = node.BreadthSearch();
+            foreach (var visitedNode in breadthSearch)
+                marked.Add(visitedNode);
+            yield return breadthSearch;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,17 +3,17 @@
     public static void Main()
     {
         var graph = Graph.MakeGraph(
-            0, 1,
-            0, 2,
-            1, 3,
-            1, 4,
-            2, 3,
-            2, 4
+            1, 2,
+            3, 4,
+            3, 5,
+            4, 5
         );
 
-        foreach (var e in graph[0].BreadthSearch())
-            Console.WriteLine(e);
-        foreach (var e in graph[0].DepthSearch())
-            Console.WriteLine(e);
+        foreach (var component in graph.FindConnectedComponents())
+        {
+            foreach (var node in component)
+                Console.WriteLine(node);
+            Console.WriteLine();
+        }
     }
 }
